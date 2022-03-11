145f9388ed581d549bdd3b7a3d9c943220983203
Martin Bruland <martinbruland@dhcp-10-24-9-100.wlan.ntnu.no>
2022-03-11 11:59:06 +0100

    private main

--- cleaner.py
+++ cleaner.py
@@ -2,10 +2,12 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 from scipy.signal import savgol_filter
-
-with open("Unknown.txt") as file:
-    df = pd.read_table(file, header=None)
-    df.columns = ['Angle', 'Intensity']
+def __main():
+    
+    with open("Unknown.txt") as file:
+        df = pd.read_table(file, header=None)
+        df.columns = ['Angle', 'Intensity']
+        print(removePeaks(df))
 
 def removePeaks(df):
     shouldRemove=set()
@@ -22,6 +24,5 @@ def removePeaks(df):
     df = df.drop(shouldRemove)
     #df["Intensity"] = savgol_filter(df["Intensity"], 5, 2)
                 
-        
-
-#print(removePeaks(df))
+if __name__=="__main__":
+    __main()
